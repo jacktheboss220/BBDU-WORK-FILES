0e426d6cfa2c410e3d289e229585e88b94a611fe
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2023-11-29 19:42:12 +0530

    12

--- php/str_functions.php
+++ php/str_functions.php
@@ -1,5 +1,12 @@
 <?php
-$name = "Mahesh Kumar";
+$name = "Mahesh Kumar is a programmer";
 
+echo "str_len: " . strlen($name) . "<br>";
+echo "str_word_count: " . str_word_count($name) . "<br>";
+echo "strrev: " . strrev($name) . "<br>";
+echo "is pos: " . strpos($name, "is") . "<br>";
+echo "is to res : " . str_replace("is", "", $name) . "<br>";
+echo "str_repeat: " . str_repeat($name, 2) . "<br>";
+echo "rtrim: " . rtrim($name) . "<br>";
 ?>
 <!--  -->
