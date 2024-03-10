9f49409e9160e8577c307ad4988e7ce5e7c091e3
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2024-03-10 21:29:56 +0530

    Delete unnecessary files and update .gitignore

--- java/loginFrom.java
+++ java/loginFrom.java
@@ -2,40 +2,40 @@ import javax.swing.*;
 
 class loginFrom {
     public static void main(String[] args) {
-        JFrame jf = new JFrame();
-        jf.setSize(300, 200);
-        jf.setLayout(null);
+        JFrame JF = new JFrame();
+        JF.setSize(300, 200);
+        JF.setLayout(null);
 
         JLabel labelTitle = new JLabel("Login Form");
         labelTitle.setBounds(100, 0, 80, 30);
-        jf.add(labelTitle);
+        JF.add(labelTitle);
 
         JLabel labelUserName = new JLabel("Username");
         labelUserName.setBounds(20, 30, 80, 30);
-        jf.add(labelUserName);
+        JF.add(labelUserName);
 
         JTextField textFieldUserName = new JTextField();
         textFieldUserName.setBounds(120, 30, 120, 30);
-        jf.add(textFieldUserName);
+        JF.add(textFieldUserName);
 
         JLabel labelPassword = new JLabel("Password");
         labelPassword.setBounds(20, 60, 80, 30);
-        jf.add(labelPassword);
+        JF.add(labelPassword);
 
         JPasswordField passwordField = new JPasswordField();
         passwordField.setBounds(120, 60, 120, 30);
-        jf.add(passwordField);
+        JF.add(passwordField);
 
         JButton buttonLogin = new JButton("Submit");
         buttonLogin.setBounds(40, 100, 80, 30);
-        jf.add(buttonLogin);
+        JF.add(buttonLogin);
 
         JButton buttonReset = new JButton("Clear");
         buttonReset.setBounds(140, 100, 80, 30);
-        jf.add(buttonReset);
+        JF.add(buttonReset);
 
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JF.setVisible(true);
+        JF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         buttonReset.addActionListener(e -> {
             textFieldUserName.setText("");
