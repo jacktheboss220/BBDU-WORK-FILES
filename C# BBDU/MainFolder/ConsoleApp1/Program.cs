6308f4cf70d9c565648a9152e35bcf51313b4a3a
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2024-03-18 17:34:46 +0530

    Add employee details and display them

--- C# BBDU/MainFolder/ConsoleApp1/Program.cs
+++ C# BBDU/MainFolder/ConsoleApp1/Program.cs
@@ -8,8 +8,27 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        String empName;
+        int empID;
+        String empAddress;
+        public Program(String name, int id, String address)
+        {
+            empName = name;
+            empID = id;
+            empAddress = address;
+        }
+        public void show()
+        {
+            Console.WriteLine("Employee Name: " + empName);
+            Console.WriteLine("Employee ID: " + empID);
+            Console.WriteLine("Employee Address: " + empAddress);
+        }
         static void Main(string[] args)
         {
+            Program p = new Program("John", 123, "New York");
+            p.show();
+            Console.ReadKey();
         }
+        
     }
 }
---
+++ java/checkBox.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class checkBox {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Feedback Form");
+        frame.setSize(400, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        JLabel name = new JLabel("Name");
+        name.setBounds(10, 10, 100, 30);
+        frame.add(name);
+
+        JTextField nameField = new JTextField();
+        nameField.setBounds(120, 10, 200, 30);
+        frame.add(nameField);
+
+        JCheckBox checkBox = new JCheckBox("Subscribe to our newsletter");
+        checkBox.setBounds(10, 90, 300, 30);
+        frame.add(checkBox);
+
+        JButton submit = new JButton("Submit");
+        submit.setBounds(10, 130, 100, 30);
+        frame.add(submit);
+
+        frame.setVisible(true);
+    }
+}
